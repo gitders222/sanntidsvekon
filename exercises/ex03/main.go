b38bab3c03259ed69fa51db1aa6b4df16613c80b
sanntid23 <v.r.roriz@gmail.com>
2017-01-25 12:57:57 +0100

    First commit ex03

--- exercises/ex03/main.go
+++ exercises/ex03/main.go
@@ -1,1 +1,31 @@
-This is a test file.
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+
+	addr,err := net.ResolveUDPAddr("udp",":30000")
+    CheckError(err)
+
+	sockln, err := net.ListenUDP("udp", addr)
+	CheckError(err)
+
+	buf := make([]byte, 1024)
+
+	for {
+		n, addr, err := sockln.ReadFromUDP(buf)
+		CheckError(err)
+		fmt.Println("Received: ", buf[0:n], "\nAddress: ", addr, "\n")
+		_ = addr
+		
+	}	
+}
+
+func CheckError(err error){
+	if err != nil {
+		// something
+	}
+}
